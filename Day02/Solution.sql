b738a05e14392e9e63a63b3ccb34afd8490889ab
Johan Mulder <johan@mumasoft.nl>
2024-12-02 19:17:12 +0100

    ~* is case insensitive
    Comment:
    https://www.reddit.com/r/adventofsql/comments/1h4pnnl/comment/m014tq4/
    
    And:
    https://www.atlassian.com/data/sql/how-regex-works-in-sql
    https://www.postgresql.org/docs/17/functions-matching.html#FUNCTIONS-POSIX-TABLE

--- Day02/Solution.sql
+++ Day02/Solution.sql
@@ -1,7 +1,7 @@
-select array_to_string(array_agg(c), '') as result
-from (select chr(value) as c
-      from letters_a
-      union all
-      select chr(value)
-      from letters_b) t
-where c ~* '[a-zA-Z\s,.!]';
+select string_agg(c, '')
+from (
+  select chr(value) as c from letters_a
+  union all
+  select chr(value) from letters_b
+) t
+where c ~* '[a-z\s,.!]';
