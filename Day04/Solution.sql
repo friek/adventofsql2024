95c7d4d6cf2d8b03f89203e2f5c487931b883ab5
Johan Mulder <johan@mumasoft.nl>
2024-12-04 20:31:08 +0100

    Added better solution

--- Day04/Solution.sql
+++ Day04/Solution.sql
@@ -21,3 +21,24 @@ from lengths
 where added_tags is not null
 order by lengths.len_added_tags desc
 ;
+
+-- Better solution from https://www.reddit.com/r/adventofsql/comments/1h62b9e/comment/m0cd9bn/
+SELECT toy_id,
+(SELECT COUNT(*)
+ FROM (
+ SELECT UNNEST(new_tags)
+ EXCEPT
+ SELECT UNNEST(previous_tags)) a) AS added,
+(SELECT COUNT(*)
+ FROM (
+ SELECT UNNEST(previous_tags)
+ INTERSECT
+ SELECT UNNEST(new_tags)) a) AS unchanged,
+(SELECT COUNT(*)
+ FROM (
+ SELECT UNNEST(previous_tags)
+ EXCEPT
+ SELECT UNNEST(new_tags)) a) AS removed
+FROM toy_production
+ORDER BY added DESC
+LIMIT 1;
