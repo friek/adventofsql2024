55b717e3dff1cbce9fa071be01ecb46de0108c7b
Johan Mulder <johan@mulder.net>
2024-12-25 11:49:24 +0100

    Added additional solution

--- Day13/Solution.sql
+++ Day13/Solution.sql
@@ -1,3 +1,4 @@
+-- My own solution
 with email_addresses as (select unnest(email_addresses) as email_address
                          from contact_list),
      domains as (select email_address, substr(email_address, position('@' in email_address) + 1) as domain
@@ -7,3 +8,11 @@ from domains
 group by domain
 order by count(*) desc
 ;
+
+-- More eloquent: https://www.reddit.com/r/adventofsql/comments/1hcz80i/comment/m1tw3ht/
+SELECT SPLIT_PART(UNNEST(email_addresses), '@', 2) AS "domain",
+       COUNT(1)                                    AS cnt
+FROM contact_list
+GROUP BY "domain"
+ORDER BY cnt DESC
+LIMIT 1;
